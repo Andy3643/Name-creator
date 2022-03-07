798c031c27fdbca729998c626c1ca41b69fdd757
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-03-07 12:10:26 +0300

    feat: add date calculator

--- script/script.js
+++ script/script.js
@@ -5,7 +5,12 @@ button.addEventListener("click",()=>{
     var male = ["Kwasi", "Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var female =["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
-
+        let dateOfBirth = document.getElementById("dob").value;
+        let monthOfBirth = document.getElementById("mob").value;
+        let yearOfBirth = document.getElementById("yob").value;
+        let gender = document.getElementById("gender").value;
+        var ourdate = new Date(yearOfBirth +'/'+monthOfBirth+'/'+dateOfBirth);
+        var day = ourdate.getDay();
 
 
 
